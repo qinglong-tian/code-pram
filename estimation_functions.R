3516cb389c0b469f073da0a32e96a37d1bdd998f
qinglong-tian <qltian2021@gmail.com>
2022-06-05 15:52:00 -0500

    remove EX

--- estimation_functions.R
+++ estimation_functions.R
@@ -1,14 +1,3 @@
-# Compute E(X)
-find_true_EX_1 <- function(yMu, ySigma, betaXY, B=10000)
-{
-  yVec <- rnorm(B, yMu, ySigma)
-  oddVec <- cbind(1, yVec) %*% matrix(betaXY, ncol = 1)
-  probVec <- 1/(1+exp(-oddVec))
-  xVec <- as.numeric(runif(B) < probVec)
-  
-  mean(xVec)
-}
-
 # Fit X*~Y
 fit_x_star_y <- function(data)
 {
